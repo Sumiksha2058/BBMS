b84d655698016a7de9b318859d671486bb6a84c4
Sumiksha2058 <npsumiksha@gmail.com>
2023-08-17 20:20:08 +0545

    new update

---
+++ Admin/functions/reject.php
@@ -0,0 +1,14 @@
+<?php
+include '../includes/config.php';
+$updateQuery = "UPDATE `donation_requests` SET `approval_status` = 'rejected' WHERE `donation_requests`.`donation_request_id` = `donation_request_id`;";
+
+if (mysqli_query($conn, $updateQuery)) {
+    // Success
+    header("Location: ../donor.php"); 
+    exit();
+} else {
+    // Error
+    echo "Error updating record: " . mysqli_error($conn);
+}
+
+?>

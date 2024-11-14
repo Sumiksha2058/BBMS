3d769562fc944dc52e922ee1f1031a3abf85339f
Sumiksha2058 <npsumiksha@gmail.com>
2024-11-14 11:39:33 +0545

    updated new database

--- vitacare_db.sql
+++ vitacare_db.sql
@@ -1,12 +1,13 @@
 -- phpMyAdmin SQL Dump
--- version 5.2.0
+-- version 5.2.1
 -- https://www.phpmyadmin.net/
 --
--- Host: localhost
--- Generation Time: Nov 06, 2023 at 10:23 AM
--- Server version: 10.4.25-MariaDB
--- PHP Version: 8.0.23
+-- Host: 127.0.0.1
+-- Generation Time: Nov 14, 2024 at 06:52 AM
+-- Server version: 10.4.28-MariaDB
+-- PHP Version: 8.2.4
 
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
 SET time_zone = "+00:00";
 
@@ -23,6 +24,34 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
+-- Table structure for table `active_donor_table`
+--
+
+CREATE TABLE `active_donor_table` (
+  `id` int(11) NOT NULL,
+  `user_id` int(11) NOT NULL,
+  `donor_name` varchar(255) DEFAULT NULL,
+  `contact_number` varchar(20) DEFAULT NULL,
+  `blood_group` varchar(5) DEFAULT NULL,
+  `message` text DEFAULT NULL,
+  `created_at` timestamp NOT NULL DEFAULT current_timestamp(),
+  `needed_time` time DEFAULT NULL,
+  `request_status` enum('accept','reject','pending') DEFAULT 'pending',
+  `recipient_name` varchar(255) DEFAULT NULL,
+  `latitude` float DEFAULT NULL,
+  `longitude` float DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `active_donor_table`
+--
+
+INSERT INTO `active_donor_table` (`id`, `user_id`, `donor_name`, `contact_number`, `blood_group`, `message`, `created_at`, `needed_time`, `request_status`, `recipient_name`, `latitude`, `longitude`) VALUES
+(1, 12, 'Test', '9818455889', 'A+', 'urgent blood needed', '2024-11-11 04:11:36', '09:48:00', 'accept', 'Sumiksha Neupane', NULL, NULL);
+
+-- --------------------------------------------------------
+
+--
 -- Table structure for table `blood_requests`
 --
 
@@ -30,26 +59,19 @@ CREATE TABLE `blood_requests` (
   `request_id` int(11) NOT NULL,
   `email` varchar(100) DEFAULT NULL,
   `requested_blood_group` varchar(5) NOT NULL,
-  `urgency` varchar(20) NOT NULL,
-  `amount_required` int(11) NOT NULL,
   `message` varchar(250) NOT NULL,
-  `request_date` timestamp NOT NULL DEFAULT current_timestamp(),
+  `created_at` timestamp NOT NULL DEFAULT current_timestamp(),
   `approval_status` enum('pending','approved','rejected') DEFAULT 'pending',
-  `user_id` int(11) DEFAULT NULL
-) ;
-
--- --------------------------------------------------------
-
---
--- Table structure for table `donors`
---
-
-CREATE TABLE `donors` (
-  `donor_id` int(11) NOT NULL,
   `user_id` int(11) NOT NULL,
-  `donation_date` date NOT NULL,
-  `blood_units_donated` int(11) NOT NULL
-) ;
+  `blood_group_id` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `blood_requests`
+--
+
+INSERT INTO `blood_requests` (`request_id`, `email`, `requested_blood_group`, `message`, `created_at`, `approval_status`, `user_id`, `blood_group_id`) VALUES
+(1, NULL, 'A+', 'Urgent! We are in need of A+ blood for a patient at local hospital who requires an immediate transfusion. If you or someone you know has this blood type, your support could make a life-saving difference. Please reach out as soon as possible. Thank yo', '2024-11-09 14:27:48', 'pending', 12, NULL);
 
 -- --------------------------------------------------------
 
@@ -65,45 +87,38 @@ CREATE TABLE `users` (
   `age` int(11) NOT NULL,
   `email` varchar(200) NOT NULL,
   `contact` varchar(200) NOT NULL,
+  `address` varchar(255) NOT NULL,
   `blood_group` varchar(150) NOT NULL,
   `password` varchar(100) NOT NULL,
   `approval_status` enum('pending','approved','rejected') DEFAULT 'pending',
-  `requested_date` date DEFAULT NULL
-) ;
-
--- --------------------------------------------------------
-
---
--- Table structure for table `vc_admin`
---
-
-CREATE TABLE `vc_admin` (
-  `admin_id` int(11) NOT NULL,
-  `a_fullname` varchar(250) NOT NULL,
-  `a_address` varchar(200) NOT NULL,
-  `a_email` varchar(200) NOT NULL,
-  `a_password` varchar(100) NOT NULL
-) ;
-
--- --------------------------------------------------------
+  `requested_date` date DEFAULT NULL,
+  `latitude` decimal(10,8) DEFAULT NULL,
+  `longitude` decimal(11,8) DEFAULT NULL,
+  `blood_group_id` int(11) DEFAULT NULL,
+  `last_seen` timestamp NOT NULL DEFAULT current_timestamp(),
+  `status` tinyint(1) DEFAULT 0,
+  `deactivation_reason` text DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
--- Table structure for table `vc_appointment`
+-- Dumping data for table `users`
 --
 
-CREATE TABLE `vc_appointment` (
-  `appo_id` int(11) NOT NULL,
-  `appo_name` varchar(50) NOT NULL,
-  `appo_email` varchar(50) NOT NULL,
-  `appo_phone` varchar(10) NOT NULL,
-  `appo_bloodtype` varchar(5) NOT NULL,
-  `appo_date` date NOT NULL,
-  `appo_time` time NOT NULL
-) ;
+INSERT INTO `users` (`user_id`, `user_type`, `fullname`, `gender`, `age`, `email`, `contact`, `address`, `blood_group`, `password`, `approval_status`, `requested_date`, `latitude`, `longitude`, `blood_group_id`, `last_seen`, `status`, `deactivation_reason`) VALUES
+(11, 'donor', 'Test', 'Female', 22, 'test@gmail.com', '9818455886', 'Nayabasti', 'A+', '$2y$10$PkKrOCeUGmUE62OK3jhDXeH.E9sZcWr6/2pD/UkhTPTv1nb3gYgce', 'pending', '2024-11-09', 27.63820000, 85.50510000, NULL, '2024-11-14 02:25:58', 1, NULL),
+(12, 'recipient', 'Sumiksha Neupane', 'Female', 33, 'npsumiksha@gmail.com', '9818455889', '', 'A+', '$2y$10$yNSVxARAHph0aIvVSuTMO.1SMRkWIoVtaWVTu30Y.Xu0QVU4EvdQu', 'pending', '2024-11-09', NULL, NULL, NULL, '2024-11-11 08:41:50', 0, 'aCCOUMT NOT NEEDED'),
+(13, 'recipient', 'Sumiksha Neupane', 'Female', 22, 'sumiksha@gmail.com', '9818455889', '', 'A+', '$2y$10$FUk29sNyLpoEgpkmOaRy7evtdAXRKP7VEzFbh1Hb2zNP1T9r7OXAC', 'pending', '2024-11-09', 27.97800000, 83.45920000, NULL, '2024-11-11 08:41:50', 0, NULL),
+(14, 'donor', 'Furba Lama', 'Female', 21, 'furba@gmail.com', '9877465647', '', 'A-', '$2y$10$vQ5u7U.hGkeLcuvqLEtX/utlKpwt2ji8Q7CmldkRaAdS.oYeGFoPa', 'pending', '2024-11-11', 27.61670000, 85.54170000, NULL, '2024-11-11 09:31:36', 1, NULL);
 
 --
 -- Indexes for dumped tables
 --
+
+--
+-- Indexes for table `active_donor_table`
+--
+ALTER TABLE `active_donor_table`
+  ADD PRIMARY KEY (`id`);
 
 --
 -- Indexes for table `blood_requests`
@@ -111,75 +126,37 @@ CREATE TABLE `vc_appointment` (
 ALTER TABLE `blood_requests`
   ADD PRIMARY KEY (`request_id`),
   ADD KEY `fk_blood_requests_recipients` (`email`),
-  ADD KEY `fk_blood_requests_users` (`user_id`);
-
---
--- Indexes for table `donors`
---
-ALTER TABLE `donors`
-  ADD PRIMARY KEY (`donor_id`),
-  ADD KEY `fk_user_id` (`user_id`);
-
---
--- Indexes for table `recipient`
---
-ALTER TABLE `recipient`
-  ADD UNIQUE KEY `unique_recp_email` (`email`);
+  ADD KEY `fk_blood_requests_users` (`user_id`),
+  ADD KEY `blood_group_id` (`blood_group_id`);
 
 --
 -- Indexes for table `users`
 --
 ALTER TABLE `users`
   ADD PRIMARY KEY (`user_id`),
-  ADD UNIQUE KEY `unique_email` (`email`);
-
---
--- Indexes for table `vc_admin`
---
-ALTER TABLE `vc_admin`
-  ADD PRIMARY KEY (`admin_id`),
-  ADD UNIQUE KEY `a_email` (`a_email`),
-  ADD UNIQUE KEY `unique_a_email` (`a_email`);
-
---
--- Indexes for table `vc_appointment`
---
-ALTER TABLE `vc_appointment`
-  ADD PRIMARY KEY (`appo_id`);
+  ADD KEY `blood_group_id` (`blood_group_id`);
 
 --
 -- AUTO_INCREMENT for dumped tables
 --
 
 --
+-- AUTO_INCREMENT for table `active_donor_table`
+--
+ALTER TABLE `active_donor_table`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
 -- AUTO_INCREMENT for table `blood_requests`
 --
 ALTER TABLE `blood_requests`
-  MODIFY `request_id` int(11) NOT NULL AUTO_INCREMENT;
-
---
--- AUTO_INCREMENT for table `donors`
---
-ALTER TABLE `donors`
-  MODIFY `donor_id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `request_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 
 --
 -- AUTO_INCREMENT for table `users`
 --
 ALTER TABLE `users`
-  MODIFY `user_id` int(11) NOT NULL AUTO_INCREMENT;
-
---
--- AUTO_INCREMENT for table `vc_admin`
---
-ALTER TABLE `vc_admin`
-  MODIFY `admin_id` int(11) NOT NULL AUTO_INCREMENT;
-
---
--- AUTO_INCREMENT for table `vc_appointment`
---
-ALTER TABLE `vc_appointment`
-  MODIFY `appo_id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `user_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=15;
 
 --
 -- Constraints for dumped tables
@@ -189,15 +166,13 @@ ALTER TABLE `vc_appointment`
 -- Constraints for table `blood_requests`
 --
 ALTER TABLE `blood_requests`
-  ADD CONSTRAINT `fk_blood_requests_recipients` FOREIGN KEY (`email`) REFERENCES `recipient` (`email`) ON DELETE CASCADE,
-  ADD CONSTRAINT `fk_blood_requests_users` FOREIGN KEY (`user_id`) REFERENCES `users` (`user_id`) ON DELETE CASCADE,
-  ADD CONSTRAINT `fkuserid` FOREIGN KEY (`user_id`) REFERENCES `users` (`user_id`);
+  ADD CONSTRAINT `blood_requests_ibfk_1` FOREIGN KEY (`blood_group_id`) REFERENCES `blood_groups` (`blood_group_id`);
 
 --
--- Constraints for table `donors`
+-- Constraints for table `users`
 --
-ALTER TABLE `donors`
-  ADD CONSTRAINT `fk_user_id` FOREIGN KEY (`user_id`) REFERENCES `users` (`user_id`) ON DELETE CASCADE;
+ALTER TABLE `users`
+  ADD CONSTRAINT `users_ibfk_1` FOREIGN KEY (`blood_group_id`) REFERENCES `blood_groups` (`blood_group_id`);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
